43d109bfe0928481823da49e827474ec8f663d85
Ahmed M2 Elgaied <46943991+ahmedelg@users.noreply.github.com>
2021-04-03 00:36:28 +0200

    Update Singleton.js

--- Singleton.js
+++ Singleton.js
@@ -4,7 +4,7 @@
   )+1 Creare only just only one instance from the class or constructor
   )+2 When they said that 'Singleton pattern' they means that class or constructor or specific structure has just one object no more. 
 */
-// IMPLEMENT SINGLETON PATTERN
+// +)1 'FIRST STYLE' > F > IMPLEMENT SINGLETON PATTERN
 var singletonPattern = ( function(){
   // +1 Declare the unique object
   var instance; // undefined
@@ -27,10 +27,24 @@ var singletonPattern = ( function(){
   }
 })();
 
-// TEST
+// )+1 TEST
 function run () {
   // Create first instance of 
   var instance1 = singletonPattern.getInstance();
   var instance2 = singletonPattern.getInstance();
   console.log(instance1 === instance2);
 }
+
+// +)2 'SECOND STYLE' > F > IMPLEMENT SINGLETON PATTERN
+function Logger() {
+  if(typeof global_log === 'undefined') {
+    global_log = this;
+  }
+  return global_log;
+}
+
+// )+2 TEST 
+var a = new Logger(); 
+var b = new Logger(); 
+console.log(a === b); // true
+console.log(a);
